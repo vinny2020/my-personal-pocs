4757813d15ebc07d10f134fa300360d7657566e1
Vincent Stoessel <backbase@Vinny-MBP.local>
2016-08-01 18:31:23 -0400

    Camel tests passing but still need to connect Camel Restlet to the Spring Boot webapp somehow

--- demomvc/src/main/java/com/xaymaca/poc/routes/XaymacaServices.java
+++ demomvc/src/main/java/com/xaymaca/poc/routes/XaymacaServices.java
@@ -3,10 +3,12 @@ package com.xaymaca.poc.routes;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Vincent Stoessel on August 01, 2016.
  */
+@Component
 public class XaymacaServices extends RouteBuilder {
 
     @Override
--- demomvc/src/test/java/com/xaymaca/poc/CamelRouteTests.java
+++ demomvc/src/test/java/com/xaymaca/poc/CamelRouteTests.java
@@ -1,14 +1,18 @@
 package com.xaymaca.poc;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Vincent Stoessel on August 01, 2016.
@@ -24,24 +28,54 @@ public class CamelRouteTests extends CamelSpringTestSupport {
                 from("direct:callMe")
                         .to("mock:received");
 
+                from("restlet:http://localhost:8080/echo-post-body?restletMethods=post")
+                        .removeHeaders("CamelHttp*")
+                        .process(new Processor() {
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                exchange.getOut().setBody(exchange.getIn().getBody());
+
+                            }
+                        })
+                        .to("mock:postPoint")  ;
+
+                from("restlet:http://localhost:8080/echo-query-string?restletMethods=get")
+                        .removeHeaders("CamelHttp*",Exchange.HTTP_QUERY )
+                        .process(new Processor() {
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+
+                                exchange.getOut().setBody(exchange.getIn().getHeader(Exchange.HTTP_QUERY));
+
+                            }
+                        })
+                        .to("mock:getPoint")  ;
+
             }
         };
+
     }
 
 
     @Test
     public void testSendAMessage() throws IOException, InterruptedException {
         MockEndpoint serviceResult = getMockEndpoint("mock:received");
-        template.sendBody("direct:callMe","Hello Moto");
+        template.sendBody("direct:callMe", "Hello Moto");
         serviceResult.expectedMessageCount(1);
-        String expectedResult = "Hello Moto" ;
-        String  resultString = (String) serviceResult.getExchanges().get(0).getIn().getBody();
+        String expectedResult = "Hello Moto";
+        String resultString = (String) serviceResult.getExchanges().get(0).getIn().getBody();
         Assert.assertEquals(expectedResult, resultString);
         serviceResult.assertIsSatisfied();
 
 
+    }
 
-
+    @Test public void testRestletConnection() throws IOException {
+        MockEndpoint serviceResult = getMockEndpoint("mock:getPoint");
+        String response = IOUtils.toString((InputStream) template.requestBody("http://localhost:8080/echo-query-string?foo=bar","")) ;
+        serviceResult.expectedMessageCount(1);
+        String expected = "foo=bar";
+        Assert.assertEquals(expected,response);
 
     }
 
