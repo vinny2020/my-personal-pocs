c885028cab0fa90fe55eec8cbf12b8154950c750
Vincent Stoessel <xaymaca@gmail.com>
2026-02-13 23:10:43 -0500

    fix: Ensure XaymacaBootServices is auto-configured with @Component

--- demomvc/src/main/java/com/xaymaca/poc/routes/XaymacaBootServices.java
+++ demomvc/src/main/java/com/xaymaca/poc/routes/XaymacaBootServices.java
@@ -6,10 +6,19 @@ import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.stereotype.Component;
+
+
+
 /**
+
  * Created by Vincent Stoessel on August 01, 2016.
+
  *
+
  */
+
+@Component
 
 public class XaymacaBootServices extends RouteBuilder {
 
