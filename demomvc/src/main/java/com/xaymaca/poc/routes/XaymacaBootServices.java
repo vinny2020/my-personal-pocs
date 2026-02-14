7c01b4d50a4f0d35876010897b01db4790283689
Vincent Stoessel <xaymaca@gmail.com>
2026-02-13 22:36:49 -0500

    feat: Upgrade Spring Boot and Apache Camel in demomvc

--- demomvc/src/main/java/com/xaymaca/poc/routes/XaymacaBootServices.java
+++ demomvc/src/main/java/com/xaymaca/poc/routes/XaymacaBootServices.java
@@ -1,12 +1,9 @@
 package com.xaymaca.poc.routes;
 
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.QueryStringDecoder;
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.Processor;
-import org.apache.camel.component.netty4.http.NettyHttpMessage;
-import org.apache.camel.spring.boot.FatJarRouter;
+import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -14,12 +11,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class XaymacaBootServices extends FatJarRouter {
+public class XaymacaBootServices extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
-        from("netty4-http:http://0.0.0.0:9090/echo-post-body")
+        from("netty-http:http://0.0.0.0:9090/echo-post-body")
                 .process(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
@@ -28,18 +25,28 @@ public class XaymacaBootServices extends FatJarRouter {
                     }
                 })
 
-                .from("netty4-http:http://0.0.0.0:9090/echo-query-string?mapHeaders=true")
+                .from("netty-http:http://0.0.0.0:9090/echo-query-string?mapHeaders=true")
                 .setExchangePattern(ExchangePattern.InOut)
                 .process(new Processor() {
                     @Override
                     public void process(Exchange exchange) throws Exception {
-
-                        FullHttpRequest nettyRequest = ((NettyHttpMessage) exchange.getIn()).getHttpRequest();
-                        QueryStringDecoder queryStringDecoder = new QueryStringDecoder(nettyRequest.getUri());
-                        String responseMapString = queryStringDecoder.parameters().toString();
-                        String response = responseMapString.replaceAll("[\\[\\]{}]", "");
-                        exchange.getIn().setBody("Hello! We found these parameters in the query string: " + response);
-
+                        StringBuilder response = new StringBuilder("Hello! We found these parameters in the query string: ");
+                        // Query parameters are mapped to headers when mapHeaders=true
+                        // They are usually prefixed with CamelHttpQuery.
+                        // Or simply CamelNettyHttp.query.
+                        // Iterate through headers and find query parameters.
+                        exchange.getIn().getHeaders().forEach((key, value) -> {
+                            if (key.startsWith("CamelHttpQuery.")) {
+                                response.append(key.substring("CamelHttpQuery.".length())).append("=").append(value).append(", ");
+                            } else if (key.startsWith("CamelNettyHttp.query.")) { // Fallback for Netty specific header name
+                                response.append(key.substring("CamelNettyHttp.query.".length())).append("=").append(value).append(", ");
+                            }
+                        });
+                        // Remove trailing ", " if any
+                        if (response.length() > "Hello! We found these parameters in the query string: ".length()) {
+                            response.setLength(response.length() - 2);
+                        }
+                        exchange.getIn().setBody(response.toString());
                     }
                 })
 
